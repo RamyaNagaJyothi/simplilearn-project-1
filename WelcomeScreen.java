635fa128d48360e7a82bb461abe6c31b5f61bc94
RamyaNagaJyothi <102315362+RamyaNagaJyothi@users.noreply.github.com>
2022-03-31 16:53:23 +0530

    Added WelcomeScreen.java

---
+++ WelcomeScreen.java
@@ -0,0 +1,14 @@
+package com.lockedme;
+
+public class WelcomeScreen {
+
+	public static void display() {
+		System.out.println("----------Welcome to the \"LockedMe\"----------");
+		System.out.println("Developer details: G.Ramya Naga Jyothi");
+		System.out.println();
+		System.out.println("----------About the application---------- ");
+		System.out.println("In the LockedMe.com, you can perform operaions like adding, deleting, searching, displaying of the files.");
+		System.out.println();
+		System.out.println();
+	}
+}
